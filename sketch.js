110a4bd17dd8a1fef5450c17458dfbcc0e876827
aith <ariiramanesh@gmail.com>
2021-02-22 18:39:49 -0800

    finished- geneation is related to language shown

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 // todo
 // add more rules
 
-let can; let canw = 700; let canh = 700;
+let can; let canw = 1600; let canh = 1000;
 
 let beat = 0;
-let fr = 30;
+let fr = 15;
 let font;
 
 let tri = "\u0394"
@@ -13,8 +13,9 @@ let rr = '\u2192' // + or rotate right
 let lb = '\u2265' // [ or start bracket
 let rb = '\u2244' // ] or start bracket
 let axiom = tri
+let dispersion = 4;
 let rules = {
-     \u0394:  [tri+tri+rr+lb+rr+tri+rl+tri+rl+tri+rb],  // triangle
+     \u0394:  [tri+tri+rr+lb+rr+tri+rl+tri+rl+tri+rb, tri+tri+tri+rr+lb+rr+tri+rl+tri+rl+tri+rb, tri+rr+lb+rr+tri+rl+tri+rl+tri+rb] // triangle
     // "\u20D4":  ["\u2228"],  // arrow (rotate left) -> self
     // "\u20D5":  ["\u2265"],  // Greater than  self
     // _:  ["\"]  // _ (end bracket) -> self
@@ -25,49 +26,52 @@ function preload() {
 }
 
 function setup() {
-    can = createCanvas(canh, canw).parent("canvasContainer")
+    can = createCanvas(canw, canh).parent("canvasContainer")
     // can.center()
     userStartAudio()
     frameRate(fr)
-    axiom = genSentence(axiom)
-    axiom = genSentence(axiom)
-    axiom = genSentence(axiom)
-    axiom = genSentence(axiom)
+    axiom = genSentence(axiom, 4)
     // can.center()
-    print(axiom)
+    // print(axiom)
     textSize(20)
-    translate(canw/2, canh/2)
-    background(000)
-    turtle(axiom)
-    document.getElementById("console").innerHTML += axiom
+    turtle(axiom);
     select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
+    document.getElementById("console").innerHTML = axiom
+    document.getElementById("console").innerHTML += axiom
+    document.getElementById("console").innerHTML += axiom
+    document.getElementById("console").innerHTML += axiom
     // createP(axiom)
 }
 
-function genSentence(axiom) {
-    print(axiom)
+function genSentence(axiom, amt) {
+    // axiom = ""
     let postman = ""
-    for (let i = 0; i < axiom.length; i++) {
-        let c = axiom.charAt(i)
-        if (!rules[c]) { postman += c; continue; }
-        else postman += rules[c]
+    axiom = tri
+    for(let outwards = 0; outwards < amt; outwards++){
+        for (let i = 0; i < axiom.length; i++) {
+            let c = axiom.charAt(i)
+            if (!rules[c]) { postman += c; continue; }
+            else postman += rules[c][random(0, 3) | 0]
+        }
+        axiom = postman
     }
-    print(postman)
     return postman
 }
 
 var len = 20;
 var height = 50;
 function turtle(axiom) {
+    let inc = 0;
     stroke("white")
     fill(000)
     calcTriDims(1)
     let angle = radians(60)
     for(let i = 0; i < axiom.length; i++) {
+        inc++;
         let c = axiom.charAt(i)
         switch(c) {
             case tri:
-                let sign = Math.round(Math.random()) * 2 - 1
+                let sign = Math.round(noise(0,inc)) * 2 - 1
                 triangle(0, 0, sign*tril, 0, sign*tri_half_l, -trih)
                 translate(-tri_half_l/2, -trih/2)
                 break;
@@ -97,9 +101,22 @@ function calcTriDims(scalar) {
     trih = Math.sqrt(tril*tril-tri_half_l*tri_half_l)
 }
 
+let seed = 1111, step = 310;
 function draw() {
+    background(000)
+    translate(canw/2+tril, canh/2)
     beat++;
-    if(beat >= 30) {beat = frameCount % 30; }
+    turtle(axiom);
+    if(beat >= 30) {
+        axiom = genSentence(axiom, dispersion)
+        document.getElementById("console").innerHTML = axiom
+        document.getElementById("console").innerHTML += axiom
+        document.getElementById("console").innerHTML += axiom
+        document.getElementById("console").innerHTML += axiom
+        beat = frameCount % 30;
+        seed += step;
+        noiseSeed(seed)
+    }
 }
 
 
